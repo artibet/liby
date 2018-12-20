940e0835b3181f139b262c4f01cc1492b496047d
Tsakiridis Sotiris <sotiris@serres.gr>
2018-12-20 12:48:43 +0200

    Import sufee dashbord into mainapp

--- liby/settings.py
+++ liby/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mainapp',
+    'psite',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Project-wide static dirs
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'framework'),
+]
--- psite/views.py
+++ psite/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Home
+def home(request):
+    return render (request, 'psite/home.html', {})
