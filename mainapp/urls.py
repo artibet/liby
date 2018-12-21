7ee1e23e306054e56e53f0dfc7542097ae4cca93
Tsakiridis Sotiris <sotiris@serres.gr>
2018-12-21 09:39:41 +0200

    Complete menus scapfolding

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,34 +1,46 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-from .views_group.books import views as book_views
-from .views_group.categories import views as category_views
-from .views_group.authors import views as author_views
-from .views_group.publishers import views as publisher_views
-from .views_group.languages import views as language_views
-from .views_group.countries import views as country_views
+
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     
-    # Εκδόσεις
-    path('books/', book_views.index, name='books-index'),
-    path('books/create/', book_views.create, name='books-create'),
-    path('books/search', book_views.search, name='books-search'),
+    # Βιβλία
+    path('books/', views.BookViews.index, name='books-index'),
+    path('books/create/', views.BookViews.create, name='books-create'),
+    path('books/search', views.BookViews.search, name='books-search'),
 
     # Κατηγοριες
-    path('categories/', category_views.index, name='categories-index'),
+    path('categories/', views.CategoryViews.index, name='categories-index'),
     
     # Συγγραφείς
-    path('authors/', author_views.index, name='authors-index'),
+    path('authors/', views.AuthorViews.index, name='authors-index'),
     
     # Εκδοτικοί οίκοι
-    path('publishers/', publisher_views.index, name='publishers-index'),
+    path('publishers/', views.PublisherViews.index, name='publishers-index'),
     
     # Γλώσσες
-    path('languages/', language_views.index, name='languages-index'),
+    path('languages/', views.LanguageViews.index, name='languages-index'),
 
     # Χώρες
-    path('countries/', country_views.index, name='countries-index'),
+    path('countries/', views.CountryViews.index, name='countries-index'),
+    
+    # Χρήστες
+    path('users/', views.UserViews.index, name='users-index'),
+    path('users/create/', views.UserViews.create, name='users-create'),
+
+    # Κρατήσεις
+    path('holds/', views.HoldViews.index, name='holds-index'),
+
+    # Δανεισμοί
+    path('lends/', views.LendViews.index, name='lends-index'),
+    path('lends/delays', views.LendViews.delays, name='lends-delays'),
+
+     # Προτάσεις
+    path('suggestions/', views.SuggestionViews.index, name='suggestions-index'),
+
+     # Σχόλια χρηστών
+    path('comments/', views.CommentViews.index, name='comments-index'),
     
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -3,3 +3,123 @@ from django.shortcuts import render
 def dashboard(request):
     return render (request, 'mainapp/dashboard.html', {})
 
+##########################################################################
+# Book views
+##########################################################################
+
+class BookViews:
+
+    def index(request):
+        return render (request, 'mainapp/books/index.html', {})
+
+    def create(request):
+        return render (request, 'mainapp/books/create.html', {})    
+
+    def search(request):
+        pass               
+
+
+##########################################################################
+# Author views
+##########################################################################
+
+class AuthorViews:
+
+    def index(request):
+        return render (request, 'mainapp/authors/index.html', {})
+
+
+##########################################################################
+# Category views
+##########################################################################
+
+class CategoryViews:
+
+    def index(request):
+        return render (request, 'mainapp/categories/index.html', {})        
+
+
+##########################################################################
+# Publisher views
+##########################################################################
+
+class PublisherViews:
+
+    def index(request):
+        return render (request, 'mainapp/publishers/index.html', {})     
+
+
+##########################################################################
+# Language views
+##########################################################################
+
+class LanguageViews:
+
+    def index(request):
+        return render (request, 'mainapp/languages/index.html', {})         
+
+
+##########################################################################
+# Country views
+##########################################################################
+
+class CountryViews:
+
+    def index(request):
+        return render (request, 'mainapp/countries/index.html', {})       
+
+
+##########################################################################
+# User views
+##########################################################################
+
+class UserViews:
+
+    def index(request):
+        return render (request, 'mainapp/users/index.html', {})
+
+    def create(request):
+        return render (request, 'mainapp/users/create.html', {})    
+
+
+##########################################################################
+# Hold views
+##########################################################################           
+
+class HoldViews:
+
+    def index(request):
+        return render (request, 'mainapp/holds/index.html', {})   
+
+
+##########################################################################
+# Lend views
+##########################################################################           
+
+class LendViews:
+
+    def index(request):
+        return render (request, 'mainapp/lends/index.html', {})      
+
+    def delays(request):
+         return render (request, 'mainapp/lends/delays.html', {})  
+
+
+##########################################################################
+# Suggestion views
+##########################################################################           
+
+class SuggestionViews:
+
+    def index(request):
+        return render (request, 'mainapp/suggestions/index.html', {})           
+
+
+##########################################################################
+# Comment views
+##########################################################################           
+
+class CommentViews:
+
+    def index(request):
+        return render (request, 'mainapp/comments/index.html', {})          
